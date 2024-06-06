6c035b8cb4c58209e1dd4595568accc309f9536b
Prajwal-Shrimali <prajwalshrimali@gmail.com>
2024-06-06 15:11:08 +0400

    Form Field Fix

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,26 +1,27 @@
-import { Text, View, ScrollView, Image, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text, View, ScrollView, Image, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { images } from '../../constants'
-import FormField from '../../components/FormField'
-import CustomButton from '../../components/CustomButton'
-import { Link, router } from 'expo-router'
-import { getCUrrentUser, signIn } from '../../lib/appwrite'
-import { useGlobalContext } from '../../context/GlobalProvider'
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+import { Link, router } from 'expo-router';
+import { getCUrrentUser, signIn } from '../../lib/appwrite';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 export default function SignIn() {
   const { setUser, setIsLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     email: '',
-    password: ''
-  })
+    password: '',
+  });
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const submit = async () => {
-    if(!form.email || !form.password) {
-      Alert.alert('Error', 'Please fill in all the fields')
-    };
+    if (!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields');
+      return;
+    }
 
     setIsSubmitting(true);
     try {
@@ -28,7 +29,7 @@ export default function SignIn() {
 
       const result = await getCUrrentUser();
       setUser(result);
-      setIsLoggedIn(true)
+      setIsLoggedIn(true);
 
       router.replace('/home');
     } catch (error) {
@@ -37,51 +38,59 @@ export default function SignIn() {
     } finally {
       setIsSubmitting(false);
     }
-  }
+  };
 
   return (
-    <SafeAreaView className='bg-primary h-full'>
+    <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className='w-full min-h-[85vh] justify-center px-4 my-6'>
-          <Image 
+        <View className="w-full min-h-[85vh] justify-center px-4 my-6">
+          <Image
             source={images.logo}
-            resizeMode='contain'
-            className='w-[115px] h-[35px]'
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
           />
-          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Login to Aora
           </Text>
 
-          <FormField 
+          <FormField
             title="Email"
             value={form.email}
-            handleChnageText={(e) => setForm({ ...form, email: e })}
-            otherStyles='mt-7'
-            keyboardType='email-address'
+            handleChangeText={(e) =>
+              setForm((prevValue) => ({ ...prevValue, email: e }))
+            }
+            otherStyles="mt-7"
+            keyboardType="email-address"
           />
-          <FormField 
+          <FormField
             title="Password"
             value={form.password}
-            handleChnageText={(e) => setForm({ ...form, password: e })}
-            otherStyles='mt-7'
+            handleChangeText={(e) =>
+              setForm((prevValue) => ({ ...prevValue, password: e }))
+            }
+            otherStyles="mt-7"
           />
 
-          <CustomButton 
-            title='Sign In'
-            handlePress = {submit}
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
           />
 
-          <View className='justify-center pt-5 flex-row gap-2'>
-            <Text className='text-lg text-gray-100 font-pregular'>
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
               Don't have an account?
             </Text>
-            <Link href='/sign-up' className='text-lg font-psemibold text-secondary-100'>Sign Up</Link>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary-100"
+            >
+              Sign Up
+            </Link>
           </View>
-
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,93 +1,94 @@
-import { Text, View, ScrollView, Image, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text, View, ScrollView, Image, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { images } from '../../constants'
-import FormField from '../../components/FormField'
-import CustomButton from '../../components/CustomButton'
-import { Link, router } from 'expo-router'
-import { createUser } from '../../lib/appwrite'
-import { useGlobalContext } from '../../context/GlobalProvider'
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+import { Link, router } from 'expo-router';
+import { createUser } from '../../lib/appwrite';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 export default function SignUp() {
   const { setUser, setIsLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     username: '',
     email: '',
-    password: ''
-  })
+    password: '',
+  });
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const submit = async () => {
-    if(!form.username || !form.email || !form.password) {
-      Alert.alert('Error', 'Please fill in all the fields')
-    };
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields');
+      return; // Early return if fields are empty
+    }
 
     setIsSubmitting(true);
     try {
       const result = await createUser(form.email, form.password, form.username);
       setUser(result);
-      setIsLoggedIn(true)
+      setIsLoggedIn(true);
 
       router.replace('/home');
     } catch (error) {
       console.log(error);
-      Alert.alert('Error', 'An error occured, please try again');
+      Alert.alert('Error', 'An error occurred, please try again');
     } finally {
       setIsSubmitting(false);
-    
     }
-  }
+  };
 
   return (
-    <SafeAreaView className='bg-primary h-full'>
+    <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className='w-full min-h-[85vh] justify-center px-4 my-6'>
-          <Image 
+        <View className="w-full min-h-[85vh] justify-center px-4 my-6">
+          <Image
             source={images.logo}
-            resizeMode='contain'
-            className='w-[115px] h-[35px]'
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
           />
-          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Sign Up to Aora
           </Text>
 
-          <FormField 
+          <FormField
             title="Username"
             value={form.username}
-            handleChnageText={(e) => setForm({ ...form, username: e })}
-            otherStyles='mt-10'
+            handleChangeText={(e) => setForm({ ...form, username: e })} // Corrected prop name
+            otherStyles="mt-10"
           />
-          <FormField 
+          <FormField
             title="Email"
             value={form.email}
-            handleChnageText={(e) => setForm({ ...form, email: e })}
-            otherStyles='mt-7'
-            keyboardType='email-address'
+            handleChangeText={(e) => setForm({ ...form, email: e })} // Corrected prop name
+            otherStyles="mt-7"
+            keyboardType="email-address"
           />
-          <FormField 
+          <FormField
             title="Password"
             value={form.password}
-            handleChnageText={(e) => setForm({ ...form, password: e })}
-            otherStyles='mt-7'
+            handleChangeText={(e) => setForm({ ...form, password: e })} // Corrected prop name
+            otherStyles="mt-7"
           />
 
-          <CustomButton 
-            title='Sign Up'
-            handlePress = {submit}
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
           />
 
-          <View className='justify-center pt-5 flex-row gap-2'>
-            <Text className='text-lg text-gray-100 font-pregular'>
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
               Have an account already?
             </Text>
-            <Link href='/sign-in' className='text-lg font-psemibold text-secondary-100'>Sign In</Link>
+            <Link href="/sign-in" className="text-lg font-psemibold text-secondary-100">
+              Sign In
+            </Link>
           </View>
-
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
